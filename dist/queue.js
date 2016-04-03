8300c09dc89e1552610eeb0791cfcd4e725c2779
Jason Raede <jason.raede@gmail.com>
2016-04-03 18:08:52 -0400

    Allow passing of metadata in sendMessage

--- dist/queue.js
+++ dist/queue.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, '__esModule', {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _get = function get(_x3, _x4, _x5) { var _again = true; _function: while (_again) { var object = _x3, property = _x4, receiver = _x5; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x3 = parent; _x4 = property; _x5 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+var _get = function get(_x4, _x5, _x6) { var _again = true; _function: while (_again) { var object = _x4, property = _x5, receiver = _x6; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x4 = parent; _x5 = property; _x6 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -349,6 +349,8 @@ var Queue = (function () {
     value: function sendMessage(data, delay) {
       var _this11 = this;
 
+      var metadata = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
       var id = undefined;
       // Insert into the jobs collection...
       return new _es6PromisePolyfill.Promise(function (resolve, reject) {
@@ -358,7 +360,8 @@ var Queue = (function () {
           status: 'queued',
           retries: 0,
           retryLimit: 10,
-          logs: []
+          logs: [],
+          metadata: metadata
         }, function (err, data) {
           if (err) reject(err);else {
             id = data.ops[0]._id;
